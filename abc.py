a189ea48334ce7d9b09868e2e49721353a9d2504
Sohail Ahmad <sobajour13@gmail.com>
2025-02-27 10:18:35 +0500

    man ftn ban lea

---
+++ abc.py
@@ -0,0 +1,14 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    name = input("Enter your name: ")
+    print(greet(name))
+
+
+print("Hello, World!")
+
+def man():
+    print("man added")
+
+man()
